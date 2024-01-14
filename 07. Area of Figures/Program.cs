e14e43e68664fb56e41bb07854f824d5abca174c
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 14:16:49 +0200

    Add files via upload

---
+++ 07. Area of Figures/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+string figure = Console.ReadLine();
+double area = 1; 
+
+if (figure == "square")
+{
+    double side = double.Parse(Console.ReadLine());
+     area = side * side;
+}   
+else if (figure == "rectangle")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    double sideB = double.Parse(Console.ReadLine());
+     area = sideA * sideB;
+}
+else if (figure == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+     area = Math.PI * radius * radius;
+}
+else if (figure == "triangle")
+{
+    double lenght = double.Parse(Console.ReadLine());
+    double height = double.Parse(Console.ReadLine());
+     area = lenght * height / 2;
+}
+
+Console.WriteLine($"{area:f3}");
